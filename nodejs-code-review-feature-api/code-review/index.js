78cae0793d059b5e828769c102bbc232eca606ba
HugoRios <hugo.rios30@gmail.com>
2020-07-20 20:58:39 -0500

    First code review and refactor

--- nodejs-code-review-feature-api/code-review/index.js
+++ nodejs-code-review-feature-api/code-review/index.js
@@ -5,18 +5,25 @@ const Weather = require("./lib/service");
 let unit = "celsius";
 
 app.get("/weather/set-units/:unit", req => {
-  unit = req.params.unit;
-  res.send("OK!");
+    unit = req.params.unit;
+    res.status(200);
+    res.send("OK!");
 });
 
-app.get("/weather/:city", (req, res) =>
-  Weather.get(req.params.city, unit).then(data => {
-    res.send(data);
-  })
+app.get("/weather/:city", (req, res, next) =>
+    Weather.get(req.params.city, unit).then(data => {
+        res.status(200);
+        res.send(data);
+    }).catch(next)
 );
 
-app.get("/weather/forecast/:city", (req, res) => {
-  Weather.getForecast(req.params.city, unit).then(data => res.send(data));
+app.get("/weather/forecast/:city", (req, res, next) => {
+    Weather.getForecast(req.params.city, unit)
+        .then(data => {
+            res.status(200);
+            res.send(data)
+        })
+        .catch(next);
 });
 
 app.listen(5000, () => console.log(`Example app listening on port 5000!`));
--- nodejs-code-review-feature-api/code-review/lib/service.js
+++ nodejs-code-review-feature-api/code-review/lib/service.js
@@ -2,79 +2,91 @@ const axios = require("axios");
 
 const apiKey = "aB729ECFE4O9";
 
+const API_URL = "http://someweatherapi.com/api/"; //use of const for api url
+
 class WeatherService {
-  get(city, unit) {
-    if (!city) throw new Error("You must provide a valid city!");
-
-    try {
-      return new Promise(async (resolve, reject) => {
-        const result = await axios.get(
-          "http://someweatherapi.com/api/" + city,
-          { apiKey }
-        );
-
-        resolve(this.processData(result, unit));
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  getCityAtTime(city, time) {
-    if (!city) throw new Error("You must provide a valid city!");
-
-    return axios.get(
-      "http://someweatherapi.com/api/" + city + "?time=" + time,
-      { apiKey }
-    );
-  }
-
-  getForecast(city, unit) {
-    let startTime = Date.now();
-    let result = [];
-
-    this.getCityAtTime(city, startTime).then(data => {
-      result.concat(this.processData(data, unit));
-
-      return this.getCityAtTime(city, startTime - 86400).then(data => {
-        result.concat(this.processData(data, unit));
-
-        return this.getCityAtTime(city, startTime - 86400 * 2).then(data => {
-          result.concat(this.processData(data, unit));
-
-          return result;
+    get(city, unit) {
+        //set whole function as a promise first to reject promise if necessary
+        return new Promise(async(resolve, reject) => {
+            if (!city) {
+                reject("You must provide a valid city!"); //reject promise if city not provided
+            }
+            try {
+                const result = await axios.get(`${API_URL}${city}`, {
+                    params: { apiKey } //specify params for better reading
+                });
+                resolve(this.processData(result, unit));
+            } catch (err) {
+                console.log(err);
+                reject(err); //log the error but also reject the promise if the api call fails
+            }
         });
-      });
-    });
-  }
-
-  processData(data, unit = "celsius") {
-    let r = [];
-
-    for (var i = 0; i < data.length; i++) {
-      const it = data[i];
-
-      if (it.isCertain) {
-        for (var j = 0; j < it.readings.length; j++) {
-          r.push({
-            time: Date.parse(it.time) + 600000 * j,
-            value:
-              unit === "celsius"
-                ? this.fToC(parseInt(it.readings[j]))
-                : parseInt(it.readings[j])
-          });
-        }
-      } else {
-        continue;
-      }
     }
 
-    return r;
-  }
+    getCityAtTime(city, time) {
+        return new Promise(async(resolve, reject) => {
+            if (!city) {
+                reject("You must provide a valid city!");
+            }
+            try { //Wrap in a try catch block in case api call fails, dont break the flow
+                const result = await axios.get(`${API_URL}${city}`, {
+                    params: {
+                        apiKey,
+                        time
+                    }
+                });
+                resolve(result)
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
+        });
+    }
 
-  fToC(t) {
-    return ((t - 32) * 5) / 9;
-  }
+    getForecast(city, unit) {
+        return new Promise((resolve, reject) => {
+            let startTime = Date.now();
+            let result = [];
+            this.getCityAtTime(city, startTime)
+                .then(data => {
+                    result.concat(this.processData(data, unit));
+                    return this.getCityAtTime(city, startTime - 86400);
+                }).then(data => {
+                    result.concat(this.processData(data, unit));
+                    return this.getCityAtTime(city, startTime - 86400 * 2);
+                })
+                .then(data => {
+                    result.concat(this.processData(data, unit));
+                    resolve(result);
+                })
+                .catch(err => {
+                    reject(err)
+                }); //chained promises will run syncronously and give the exact response at index controller
+        });
+    }
+
+    processData(data, unit = "celsius") {
+        let response = []; //change variable name to not confuse
+
+        for (let item of data) { //minimize code by using for( item of data)
+            if (item.isCertain) {
+                item.readings.forEach((r) => { //using foreach loop to minimize code
+                    response.push({
+                        time: Date.parse(item.time) + 600000 * r,
+                        value: unit !== "celsius" ? this.fToC(parseInt(r)) : parseInt(r) // use different in order to make conversion correctly
+                    })
+                })
+            } else {
+                continue;
+            }
+        }
+
+        return response;
+    }
+
+    fToC(t) {
+        return ((t - 32) * 5 / 9); //use of parenthesis could cause different result
+    }
 }
 
 module.exports = new WeatherService();
